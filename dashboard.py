1bdfc18f44c748128f6ea838a5a808c23ecd1ee9
Amanuel Wolde-Kidan <amanuel.woldekidan@valyria.de>
2022-03-13 19:05:38 +0100

    WIP: building nicer rundown

--- dashboard.py
+++ dashboard.py
@@ -1,18 +1,44 @@
 import pathlib
-import numpy as np
 import pandas as pd
 import streamlit as st
 
-# load data from excel files
-data_path = pathlib.Path(__file__).parent / 'data'
-excel_files = list(data_path.glob('*.xlsx'))
-# store all dates and game data
-game_data = [pd.read_excel(e, sheet_name=['Basics', 'TeamA', 'TeamB', 'Rundown'])
-             for e in excel_files]
-dates = pd.Series([pd.to_datetime(e.name.split('_')[0], dayfirst=True) for e in excel_files])
-# sort by date
-dates.sort_values(inplace=True)
-game_data = [game_data[i] for i in dates.index]
+
+@st.cache()
+def load_data(data_path: pathlib.Path = pathlib.Path(__file__).parent / 'data'):
+    """
+    Load data from excel files.
+    """
+    excel_files = list(data_path.glob('*.xlsx'))
+    # store all dates and game data
+    game_data = [pd.read_excel(e, sheet_name=['Basics', 'TeamA', 'TeamB', 'Rundown']) for e in excel_files]
+    dates = pd.Series([pd.to_datetime(e.name.split('_')[0], dayfirst=True) for e in excel_files])
+    # sort by date
+    dates.sort_values(inplace=True)
+    game_data = [game_data[i] for i in dates.index]
+
+    return dates, game_data
+
+
+@st.cache()
+def buil_rundown(raw_rundown, teamA, teamB, teamA_data, teamB_data):
+    """
+    Build markdown match rundown.
+    """
+    nicer_rundown = []
+    minute = 0
+    for row in raw_rundown.iterrows():
+        if row[1].Minute:  # gather current minute
+            minute = row[1].Minute
+        if row[1]['# A'] >= 0:  # gather player and team name
+            name = teamA_data[row[1]['# A'] == teamA_data['#']].Name
+            team = teamA
+        else:
+            name = teamB_data[row[1]['# B'] == teamB_data['#']].Name
+            team = teamB
+        #  gather scoring type
+        # TODO: check if 3pointer, free throw or regular basket was made
+        #  by looking at point differential, might include emojis for this
+
 
 
 # build streamlit page
@@ -25,14 +51,16 @@ with st.container():
         st.image('logo.jpg', use_column_width=True)
 
 # build sidebar
+dates, game_data = load_data()
 st.sidebar.title('Flamingo Fadaways 🦩')
 dates_str = [d.strftime("%d.%m.%Y") for d in dates]
-game_selector = st.sidebar.selectbox('Spieldatum', options=dates_str)
+game_selector = st.sidebar.selectbox('Matchday', options=dates_str)
 game_idx = [i for i, d in enumerate(dates_str) if d in game_selector][0]
 
 # display selected game data
-for dat_lbl in ['Basics', 'TeamA', 'TeamB']: # ['Basics', 'TeamA', 'TeamB', 'Rundown'],
+for dat_lbl in ['Basics', 'TeamA', 'TeamB']:
     st.dataframe(game_data[game_idx][dat_lbl])
+
+
 # TODO:
-# figure out how to display 'Rundown' Table,
-# '-' character for free throws is not supported since column dtypes need to be uniform...
+# display rundown using, build rundown function and markdown formatting
