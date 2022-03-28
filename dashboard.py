7a2208b688718d98aef1f6ed32c36e86dc4c21ff
Amanuel Wolde-Kidan <amanuel.woldekidan@valyria.de>
2022-03-28 14:17:16 +0200

    WIP: change data storage structure

--- dashboard.py
+++ dashboard.py
@@ -8,15 +8,18 @@ def load_data(data_path: pathlib.Path = pathlib.Path(__file__).parent / 'data'):
     """
     Load data from excel files.
     """
-    excel_files = list(data_path.glob('*.xlsx'))
+    game_path = data_path / 'games/'
+    excel_files = list(game_path.glob('*.xlsx'))
     # store all dates and game data
     game_data = [pd.read_excel(e, sheet_name=['Basics', 'TeamA', 'TeamB', 'Rundown']) for e in excel_files]
-    dates = pd.Series([pd.to_datetime(e.name.split('_')[0], dayfirst=True) for e in excel_files])
+    dates = pd.Series([pd.to_datetime(e.name.split('_')[-1].split('.')[0], dayfirst=True) for e in excel_files])
     # sort by date
     dates.sort_values(inplace=True)
     game_data = [game_data[i] for i in dates.index]
+    # read roster
+    roster = pd.read_excel(data_path / 'roster.xlsx')
 
-    return dates, game_data
+    return dates, game_data, roster
 
 
 # @st.cache()
@@ -37,7 +40,7 @@ def build_rundown(game_data):
         if row[1]['# A'] >= 0:  # gather player and team name
             whoscored = 'A'
             team = teamA
-            assert (row[1]['# A'] == teamA_data['#']).sum(), f'Error: Wrong player number in minute {minute}!'
+            assert (row[1]['# A'] == teamA_data['#']).sum(), f'Error: Wrong player numbeexitr in minute {minute}!'
             name = teamA_data[row[1]['# A'] == teamA_data['#']].Name.values[0]
         elif row[1]['# B'] >= 0:
             whoscored = 'B'
@@ -66,16 +69,49 @@ def build_rundown(game_data):
     return nicer_rundown
 
 
+def print_game_stats(game_idx):
+    """
+    Display nice game stats.
+    """
+    # generate rundown
+    rundown = build_rundown(game_data[game_idx])
+
+    # display selected game data
+    with st.container():
+        st.dataframe(game_data[game_idx]['Basics'])
+        col1, col2 = st.columns(2)
+        for col, team in zip([col1, col2], ['TeamA', 'TeamB']):
+            with col:
+                st.dataframe(game_data[game_idx][team])
+    # print rundown
+    with st.container():
+        for line in rundown:
+            st.write(line)
+
+    # TODO: make this look nicer
+
+
+def print_player_stats(player):
+    """
+    Display player stats.
+    """
+    st.write(f'{player} Player Stats')
+    # TODO: calculate player stats
+
+
 # build streamlit page
 st.set_page_config(page_title="Flamingo Fadaways", page_icon="🦩", layout="wide", initial_sidebar_state="expanded",
                    menu_items={'About': "### Source Code on [Github](https://github.com/woldeaman/flamingo_stats)"})
 
 # build sidebar
-dates, game_data = load_data()
+dates, game_data, roster = load_data()
 st.sidebar.title('Flamingo Fadaways 🦩')
 dates_str = [d.strftime("%d.%m.%Y") for d in dates]
-game_selector = st.sidebar.selectbox('Matchday', options=dates_str)
+game_selector = st.sidebar.selectbox('Matchday Stats', options=dates_str)
+show_match = st.sidebar.button('Show', key='show_match')
 game_idx = [i for i, d in enumerate(dates_str) if d in game_selector][0]
+player_selector = st.sidebar.selectbox('Player Stats', options=roster['Name'].values)
+show_player = st.sidebar.button('Show', key='show_player')
 
 # setup main page with logo and average stats
 with st.container():
@@ -87,12 +123,8 @@ with st.container():
     col2.metric("PPG", "47 pts", "-10 pts")
     col3.metric("FT%", "55%", "5%")
 
-# compute nicer rundown
-rundown = build_rundown(game_data[game_idx])
-
-with st.container():
-    # display selected game data
-    for dat_lbl in ['Basics', 'TeamA', 'TeamB']:
-        st.dataframe(game_data[game_idx][dat_lbl])
-    for line in rundown:
-        st.write(line)
+# print either game rundown or player stats, depending on selection
+if show_match:
+    print_game_stats(game_idx)
+if show_player:
+    print_player_stats(player_selector)
