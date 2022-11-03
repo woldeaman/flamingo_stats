9a146a1327ec4292a2383d603fc2746e64fa185c
Amanuel Wolde-Kidan <amanuel.woldekidan@valyria.de>
2022-11-03 16:01:38 +0100

    [WIP] improve rundown info

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,5 @@
 import pathlib
+import numpy as np
 import pandas as pd
 import streamlit as st
 
@@ -33,14 +34,25 @@ def build_rundown(game_data):
     minute = 0
     score = {'A': 0, 'B': 0}
     whoscored = ''
-    # TODO: include fouls and quarters in rundown
+
     for row in raw_rundown.iterrows():
         if row[1].Minute >= 0:  # gather current minute
             minute = row[1].Minute
-        if row[1]['# A'] >= 0:  # gather player and team name
+
+        # look for fouls in this minute
+        fouled_A = (teamA_data.iloc[:, 3:] == minute).sum(1).astype(bool)
+        fouled_B = (teamB_data.iloc[:, 3:] == minute).sum(1).astype(bool)
+        if fouled_A.sum() or fouled_B.sum():  # if multiple players fouled in the same min, this will only print the 1st
+            player_fouled = teamA_data[fouled_A].Name.values[0] if fouled_A.sum() else teamB_data[fouled_B].Name.values[0]
+            team_fouled = teamA if fouled_A.sum() else teamB
+            foul_line = f'{int(minute):02d}: &nbsp; {team_fouled}: {player_fouled} commited a foul 🚨'
+            if foul_line not in nicer_rundown:  # only print once
+                nicer_rundown.append(foul_line)
+
+        if row[1]['# A'] >= 0:  # gather scoring player and team name
             whoscored = 'A'
             team = teamA
-            assert (row[1]['# A'] == teamA_data['#']).sum(), f'Error: Wrong player numbeexitr in minute {minute}!'
+            assert (row[1]['# A'] == teamA_data['#']).sum(), f'Error: Wrong player number in minute {minute}!'
             name = teamA_data[row[1]['# A'] == teamA_data['#']].Name.values[0]
         elif row[1]['# B'] >= 0:
             whoscored = 'B'
@@ -49,6 +61,7 @@ def build_rundown(game_data):
             name = teamB_data[row[1]['# B'] == teamB_data['#']].Name.values[0]
         else:  # catching second free throw case
             pass  # same player and team scores
+
         #  gather scoring type
         points = row[1][f'Score {whoscored}'] - score[whoscored] if str(row[1][f'Score {whoscored}']).isdigit() else 0
         if points == 3:
@@ -61,34 +74,71 @@ def build_rundown(game_data):
             assert row[1][f'Score {whoscored}'] in '-', 'Error: No valid number of points made but also no sign for missed freethrow!'
             play = 'missed a free throw 🧱'
         score[whoscored] += points
+
         # make entry for nicer rundown
         scoreA, scoreB = score.values()
         line = f'{int(minute):02d}: &nbsp; {int(scoreA):02d}:{int(scoreB):02d} &nbsp; {team}: {name} {play}'
         nicer_rundown.append(line)
 
+        # add line for end of quarter
+        if minute % 10 == 0 and minute < 40:
+            position = {1: 'st', 2: 'nd', 3: 'rd'}
+            qtr = int(minute // 10)
+            qtr_line = f'----- &nbsp; End of {qtr}{position[qtr]} quarter - Score: {int(scoreA):02d}:{int(scoreB):02d} &nbsp; -----'
+            if raw_rundown.iloc[row[0] + 1].Minute > minute:  # only print at the end
+                nicer_rundown.append(qtr_line)
+
+    # add lines for end of game
+    winner = teamA if scoreA > scoreB else teamB
+    end = [f'----- &nbsp; End of 4th quarter - Score: {int(scoreA):02d}:{int(scoreB):02d} &nbsp; -----',
+           f'----- &nbsp; End of Game, Team {winner} wins: {int(scoreA):02d}:{int(scoreB):02d} &nbsp; -----']
+    nicer_rundown += end
+
     return nicer_rundown
 
 
-def print_game_stats(game_idx):
+def print_game_stats(game_dat):
     """
     Display nice game stats.
     """
     # generate rundown
-    rundown = build_rundown(game_data[game_idx])
+    rundown = build_rundown(game_dat)
 
     # display selected game data
     with st.container():
-        st.dataframe(game_data[game_idx]['Basics'])
+        st.dataframe(game_dat['Basics'])
         col1, col2 = st.columns(2)
         for col, team in zip([col1, col2], ['TeamA', 'TeamB']):
             with col:
-                st.dataframe(game_data[game_idx][team])
+                st.dataframe(game_dat[team])
     # print rundown
     with st.container():
         for line in rundown:
             st.write(line)
+        # make_game_plot(rundown, game_dat['Basics']['Name'][0], game_dat['Basics']['Name'][1])
 
     # TODO: make this look nicer
+
+
+# def make_game_plot(rundown, teamA, teamB):
+#     """
+#     Make a bar plot for the game rundown.
+#     """
+#     # generate data from rundown
+#     seriesA, seriesB = pd.DataFrame(columns=['min', 'scoreA']), pd.DataFrame(columns=['min', 'scoreB'])
+#     for i, line in enumerate(rundown[1:]):
+#         min = int(line[:2])
+#         if teamA in line:
+#             seriesA.loc[i, 'min'] = min
+#             seriesA.loc[i, 'scoreA'] = int(line[3:].split(':')[0][-3:])
+#         elif teamB in line:
+#             seriesB.loc[i, 'min'] = min
+#             seriesB.loc[i, 'scoreB'] = int(line[3:].split(':')[1][:3])
+
+#     plot_df = pd.DataFrame(np.array([np.array(scoreA), -np.array(scoreB)]).T,
+#                            columns=['scoreA', 'scoreB'],
+#                            index=np.array(minutes))
+#     st.bar_chart(plot_df)
 
 
 def print_player_stats(player):
@@ -125,6 +175,6 @@ with st.container():
 
 # print either game rundown or player stats, depending on selection
 if show_match:
-    print_game_stats(game_idx)
+    print_game_stats(game_data[game_idx])
 if show_player:
     print_player_stats(player_selector)
